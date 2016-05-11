1eccada49b8c8246199c2c7f830ca75bf2084d04
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-05-11 12:21:17 +0100

    Added total row

--- GeminiBacklog/GeminiBacklog/Controllers/LoggedTimesController.cs
+++ GeminiBacklog/GeminiBacklog/Controllers/LoggedTimesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Web.Http;
 using Dapper;
@@ -26,21 +27,42 @@ namespace GeminiBacklog.Controllers
         [Route("loggedtimes/{userId}/{startDate}")]
         public dynamic Get(int userId, DateTime startDate)
         {
-            dynamic results = null;
-            using (var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                results = new
+            var historyItems = HistoryItems(userId, startDate);
+            return new
                 {
                     UserId = userId,
                     StartDate = startDate,
-                    History = sqlConnection.Query<HistoryModel>(_sql, new { UserId = userId, StartDate = startDate })
+                    History = historyItems,
+                    Total = new
+                    {
+                        Monday = historyItems.Sum(items => items.Monday),
+                        Tuesday = historyItems.Sum(items => items.Tuesday),
+                        Wednesday = historyItems.Sum(items => items.Wednesday),
+                        Thursday = historyItems.Sum(items => items.Thursday),
+                        Friday = historyItems.Sum(items => items.Friday)
+                    }
                 };
+            }
+
+        IEnumerable<HistoryModel> HistoryItems(int userId, DateTime startDate)
+        {
+            IEnumerable<HistoryModel> results = null;
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                results =
+                    sqlConnection
+                        .Query<HistoryModel>(_sql, new { UserId = userId, StartDate = startDate })
+                        .OrderByDescending(items => items.Monday)
+                        .ThenByDescending(items => items.Tuesday)
+                        .ThenByDescending(items => items.Wednesday)
+                        .ThenByDescending(items => items.Thursday)
+                        .ThenByDescending(items => items.Friday);
                 sqlConnection.Close();
             }
             return results;
-        }
-     
+    }
+    
         public class HistoryModel
         {
             public string Issue { get; set; }
