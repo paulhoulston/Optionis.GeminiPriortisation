e3864aaa224c92a399c681cdc99d9c261a7e7961
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-05-11 11:43:39 +0100

    Moved permissible dates into separate controller

---
+++ GeminiBacklog/GeminiBacklog/Controllers/LoggedTimesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Web.Http;
+using Dapper;
+
+namespace GeminiBacklog.Controllers
+{
+    public class LoggedTimesController : ApiController
+    {
+        static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Gemini"].ConnectionString;
+        static readonly string _sql;
+
+        static LoggedTimesController()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GeminiBacklog.Queries.WorkHistory.sql"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                _sql = reader.ReadToEnd();
+            }
+        }
+
+        [Route("loggedtimes/{userId}")]
+        public dynamic Get(int userId)
+        {
+            dynamic results = null;
+            var firstDayOfWeek = FirstDayOfWeek();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                results = new
+                {
+                    UserId = userId,
+                    StartDate = firstDayOfWeek,
+                    History = sqlConnection.Query<HistoryModel>(_sql, new { UserId = userId, StartDate = firstDayOfWeek })
+                };
+                sqlConnection.Close();
+            }
+            return results;
+        }
+        
+        DateTime FirstDayOfWeek()
+        {
+            var today = DateTime.Today;
+            return DateTime.Today.AddDays(1 - (int)today.DayOfWeek);
+        }
+
+        public class HistoryModel
+        {
+            public string Issue { get; set; }
+            public string GeminiRef { get; set; }
+            public int Monday { get; set; }
+            public int Tuesday { get; set; }
+            public int Wednesday { get; set; }
+            public int Thursday { get; set; }
+            public int Friday { get; set; }
+        }
+    }
+}
---
+++ GeminiBacklog/GeminiBacklog/Controllers/PermissibleDatesController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Web.Http;
+
+namespace GeminiBacklog.Controllers
+{
+    public class PermissibleDatesController : ApiController
+    {
+        [Route("permissibledates")]
+        public dynamic Get()
+        {
+            var theDate = FirstDayOfWeek();
+            var dates = new DateField[25];
+            for (var i = 0; i < 25; i++)
+            {
+                dates[i] = new DateField(theDate.AddDays(-7 * i));
+            }
+            return dates;
+        }
+
+        DateTime FirstDayOfWeek()
+        {
+            var today = DateTime.Today;
+            return DateTime.Today.AddDays(1 - (int)today.DayOfWeek);
+        }
+
+        class DateField
+        {
+            public DateField(DateTime theDate)
+            {
+                DateText = theDate.ToString("dd/MM/yyyy");
+                DateValue = theDate.ToString("dd-MMM-yyyy");
+
+            }
+            public string DateText { get; set; }
+            public string DateValue { get; set; }
+        }
+    }
+}
