c04d0b7f3b55d0c320db1f5bc2f41acf03205038
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-05-11 11:12:19 +0100

    Added SQL query to bring back history

--- GeminiBacklog/GeminiBacklog/LoggedTimesController.cs
+++ GeminiBacklog/GeminiBacklog/LoggedTimesController.cs
@@ -1,16 +1,62 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Web.Http;
+using Dapper;
 
 namespace GeminiBacklog
 {
     public class LoggedTimesController : ApiController
     {
+        static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Gemini"].ConnectionString;
+        static readonly string _sql;
+
+        static LoggedTimesController()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GeminiBacklog.Queries.WorkHistory.sql"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                _sql = reader.ReadToEnd();
+            }
+        }
+
         [Route("loggedtimes/{userId}")]
         public dynamic Get(int userId)
         {
-            return new[]
+            dynamic results = null;
+            var firstDayOfWeek = FirstDayOfWeek();
+            using (var sqlConnection = new SqlConnection(_connectionString))
             {
-                userId
-            };
+                sqlConnection.Open();
+                results = new
+                {
+                    UserId = userId,
+                    StartDate = firstDayOfWeek,
+                    History = sqlConnection.Query<HistoryModel>(_sql, new { UserId = 81, StartDate = firstDayOfWeek })
+                };
+                sqlConnection.Close();
+            }
+            return results;
+        }
+
+        DateTime FirstDayOfWeek()
+        {
+            var today = DateTime.Today;
+            return DateTime.Today.AddDays(1 - (int)today.DayOfWeek);
+        }
+
+        public class HistoryModel
+        {
+            public string UserName { get; set; }
+            public string Issue { get; set; }
+            public string GeminiRef { get; set; }
+            public string Monday { get; set; }
+            public string Tuesday { get; set; }
+            public string Wednesday { get; set; }
+            public string Thursday { get; set; }
+            public string Friday { get; set; }
         }
     }
 }
---
+++ GeminiBacklog/GeminiBacklog/Queries/WorkHistory.sql
@@ -0,0 +1,35 @@
+﻿SELECT
+	CAST(tt.timeentrydate AS DATE) AS WorkDate,
+	u.firstname + ' ' + u.surname AS UserName,
+	CASE
+		WHEN i.projid = 12 THEN 'T-' + CAST(i.issueid AS VARCHAR)
+		WHEN i.projid = 14 THEN 'CR-' + CAST(i.issueid AS VARCHAR)
+	END AS GeminiRef,
+	i.summary AS Issue,
+	SUM(tt.hours*60 + tt.minutes) AS WorkTimeInMinutes
+INTO #TimeLogs
+FROM
+	[dbo].[timetracking] tt
+JOIN dbo.users u ON u.userid = tt.userid
+JOIN 	dbo.issues i ON i.issueid = tt.issueid
+WHERE tt.timeentrydate >= @StartDate
+AND tt.timeentrydate < DATEADD(DAY, 7, @StartDate)
+AND	u.userid = @UserId
+GROUP BY u.firstname, u.surname, i.projid, i.issueid, i.summary, tt.timeentrydate
+
+SELECT UserName, Issue, GeminiRef, SUM(Monday) AS Monday, SUM(Tuesday) AS Tuesday, SUM(Wednesday) AS Wednesday, SUM(Thursday) AS Thursday, SUM(Friday) AS Friday
+FROM (
+	SELECT UserName, Issue, GeminiRef, WorkTimeInMinutes AS Monday, 0 AS Tuesday, 0 AS Wednesday, 0 AS Thursday, 0 AS Friday FROM #TimeLogs WHERE WorkDate = @StartDate
+	UNION ALL
+	SELECT UserName, Issue, GeminiRef, 0 AS Monday, WorkTimeInMinutes AS Tuesday, 0 AS Wednesday, 0 AS Thursday, 0 AS Friday FROM #TimeLogs WHERE WorkDate = DATEADD(DAY, 1, @StartDate)
+	UNION ALL
+	SELECT UserName, Issue, GeminiRef, 0 AS Monday, 0 AS Tuesday, WorkTimeInMinutes AS Wednesday, 0 AS Thursday, 0 AS Friday FROM #TimeLogs WHERE WorkDate = DATEADD(DAY, 2, @StartDate)
+	UNION ALL
+	SELECT UserName, Issue, GeminiRef, 0 AS Monday, 0 AS Tuesday, 0 AS Wednesday, WorkTimeInMinutes AS Thursday, 0 AS Friday FROM #TimeLogs WHERE WorkDate = DATEADD(DAY, 3, @StartDate)
+	UNION ALL
+	SELECT UserName, Issue, GeminiRef, 0 AS Monday, 0 AS Tuesday, 0 AS Wednesday, 0 AS Thursday, WorkTimeInMinutes AS Friday FROM #TimeLogs WHERE WorkDate = DATEADD(DAY, 4, @StartDate)
+) AS t
+GROUP BY t.UserName, t.Issue, t.GeminiRef
+ORDER BY UserName
+
+DROP TABLE #TimeLogs
