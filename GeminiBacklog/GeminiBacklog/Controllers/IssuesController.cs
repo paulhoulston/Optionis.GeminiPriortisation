70d6f5258975581714a3dfb2d7e237c9153e74c1
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-06-17 09:59:24 +0100

    Filtered out admin tasks from assigned tasks for people

--- GeminiBacklog/GeminiBacklog/Controllers/IssuesController.cs
+++ GeminiBacklog/GeminiBacklog/Controllers/IssuesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
 using System.Web.Http;
 using Dapper;
 using GeminiBacklog.Models;
@@ -9,29 +11,17 @@ namespace GeminiBacklog.Controllers
     public class IssuesController : ApiController
     {
         static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Gemini"].ConnectionString;
-        static readonly string _sql = "SELECT " +
-                                      " i.issueId AS IssueId," +
-                                      " CASE " +
-                                      "     WHEN i.projid = 12 THEN 'T-' + CONVERT(NVARCHAR(10), i.issueid) " +
-                                      "		WHEN i.projid = 14 THEN 'CR-' + CONVERT(NVARCHAR(10), i.issueid) " +
-                                      "		ELSE 'PROB-' + CONVERT(NVARCHAR(10), i.issueid) " +
-                                      "	END AS Issue, " +
-                                      " i.projid AS Project, " +
-                                      "	i.summary AS Summary, " +
-                                      "	isl.statusdesc AS[Status], " +
-                                      "    itl.typedesc AS[Type], " +
-                                      "    CASE WHEN i.duedate IS NULL THEN '' ELSE CONVERT(VARCHAR(10), i.duedate, 103) END AS DueDate, " +
-                                      "    CONVERT(VARCHAR(10), i.created, 103) AS Created, " +
-                                      "    REPLACE(REPLACE(REPLACE(REPLACE(p.prioritydesc, '1 - ', ''), '2 - ', ''), '3 - ', ''), '4 - ', '') AS[Priority] " +
-                                      "FROM dbo.issueresource ir " +
-                                      "JOIN dbo.issues i on ir.issueid = i.issueid " +
-                                      "JOIN dbo.issuestatuslut isl on isl.statusid = i.issstatus " +
-                                      "JOIN dbo.issuetypelut itl on itl.typeid = i.isstype " +
-                                      "JOIN dbo.issueprioritylut p on p.priorityid = i.isspriority " +
-                                      "WHERE userid = @userId " +
-                                      " AND isl.statusdesc NOT IN ('Closed') " +
-                                      "ORDER BY i.issueid";
-        
+        static readonly string _sql;
+
+        static IssuesController()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GeminiBacklog.Queries.AssignedIssues.sql"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                _sql = reader.ReadToEnd();
+            }
+        }
+
         [Route("issues/{userId}")]
         public dynamic Get(int userId)
         {
