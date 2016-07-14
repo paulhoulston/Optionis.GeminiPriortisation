4ba37fa59d84f1c603cd468d3f6ac4247f193a89
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-07-14 13:18:17 +0100

    Added application enhancements and bau queries and controller actions

---
+++ GeminiBacklog/GeminiBacklog/Controllers/ApplicationEnhancementsController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Web.Http;
+using Dapper;
+using GeminiBacklog.Models;
+
+namespace GeminiBacklog.Controllers
+{
+    public class ApplicationEnhancementsController : ApiController
+    {
+        static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Gemini"].ConnectionString;
+        static readonly string _sql;
+
+        static ApplicationEnhancementsController()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GeminiBacklog.Queries.ApplicationEnhancements.sql"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                _sql = reader.ReadToEnd();
+            }
+        }
+
+        // GET api/<controller>
+        public dynamic Get()
+        {
+            dynamic results = null;
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                results = new { Issues = sqlConnection.Query<IssueModel>(_sql) };
+                sqlConnection.Close();
+            }
+            return results;
+        }
+    }
+
+    public class BAUTasksController : ApiController
+    {
+        static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Gemini"].ConnectionString;
+        static readonly string _sql;
+
+        static BAUTasksController()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GeminiBacklog.Queries.BAUTasks.sql"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                _sql = reader.ReadToEnd();
+            }
+        }
+
+        // GET api/<controller>
+        public dynamic Get()
+        {
+            dynamic results = null;
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                results = new { Issues = sqlConnection.Query<IssueModel>(_sql) };
+                sqlConnection.Close();
+            }
+            return results;
+        }
+    }
+}
---
+++ GeminiBacklog/GeminiBacklog/Queries/BAUTasks.sql
@@ -0,0 +1,86 @@
+﻿SELECT        
+       i.issueid AS IssueId,
+       CASE
+            WHEN i.projid = 12 THEN 'T-' + CONVERT(NVARCHAR(10), i.issueid)
+            WHEN i.projid = 14 THEN 'CR-' + CONVERT(NVARCHAR(10), i.issueid)
+            ELSE 'PROB-' + CONVERT(NVARCHAR(10), i.issueid)
+       END AS Issue, 
+       i.summary AS Summary, 
+       CASE   
+              WHEN isl.statusdesc IN ('Pending Closure','For Release') THEN 1
+              WHEN isl.statusdesc LIKE ('%UAT%') THEN 2
+              WHEN isl.statusdesc LIKE ('%Testing%') THEN 3
+              WHEN isl.statusdesc In ('Logged with supplier', 'waiting on customer') THEN 4
+              WHEN isl.statusdesc In  ('In Progress', 'Development')THEN 5
+              WHEN isl.statusdesc In ('Development Pending', 'Design Pending') THEN 6
+              WHEN isl.statusdesc LIKE ('%analysis%') THEN 7
+              ELSE 99
+       END AS StatusOrder,
+       isl.statusdesc AS Status, 
+       itl.typedesc AS Type, 
+       CASE 
+              WHEN i.duedate < (GETDATE()) THEN '1 - Overdue' 
+              WHEN i.duedate between GETDATE () AND (GETDATE() + 28) THEN '2 - Due Soon'
+              ELSE '3 - Not Due' 
+       END AS 'DueSoon?', 
+       i.duedate AS DueDate, 
+       i.created AS Created, 
+       p.prioritydesc AS PriorityDesc, 
+       i.estimatedays AS EstDays, 
+    i.estimatehours AS EstHours, 
+       c.fielddata AS 'IT Owner'
+FROM            
+       issueresource AS ir 
+       INNER JOIN issues AS i ON ir.issueid = i.issueid 
+       INNER JOIN issuestatuslut AS isl ON isl.statusid = i.issstatus 
+       INNER JOIN issuetypelut AS itl ON itl.typeid = i.isstype 
+       INNER JOIN issueprioritylut AS p ON p.priorityid = i.isspriority 
+       INNER JOIN customfielddata AS c ON i.issueid = c.issueid
+WHERE  
+       c.customfieldid = 34     
+       AND i.projid = 14 
+       -- AND (ir.userid NOT IN (60, 79))
+
+GROUP BY 
+       i.issueid,
+       i.projid,
+       i.summary, 
+       isl.statusdesc, 
+       itl.typedesc, 
+       i.duedate, 
+       i.created, 
+       p.prioritydesc, 
+       i.estimatedays, 
+       i.estimatehours, 
+       c.fielddata, 
+       CASE 
+              WHEN i.duedate < (GETDATE()) THEN '1 - Overdue' 
+              WHEN i.duedate between GETDATE () AND (GETDATE() + 28) THEN '2 - Due Soon'
+              ELSE '3 - Not Due' 
+       END
+
+
+HAVING        
+       (NOT (isl.statusdesc IN (N'Closed', N'On Hold'))) 
+       AND (NOT (i.issueid IN (29017, 29015, 31860, 44238, 44239, 44227, 44034))) 
+       AND (itl.typedesc IN (N'BAU Applications Support','BUG') )
+       
+ORDER BY 
+       'DueSoon?',
+       CASE 
+              WHEN i.duedate < (GETDATE()) THEN i.duedate
+              WHEN i.duedate between GETDATE () AND (GETDATE() + 28) THEN i.duedate
+              ELSE NULL
+       END,
+       PriorityDesc, 
+       CASE 
+              WHEN isl.statusdesc IN ('Pending Closure', 'For Release') THEN 1 
+              WHEN isl.statusdesc LIKE ('%UAT%') THEN 2 
+              WHEN isl.statusdesc LIKE ('%Testing%') THEN 3 
+              WHEN isl.statusdesc IN ('Logged with supplier', 'waiting on customer') THEN 4 
+              WHEN isl.statusdesc IN ('In Progress', 'Development') THEN 5 
+              WHEN isl.statusdesc IN ('Development Pending', 'Design Pending') THEN 6 
+              WHEN isl.statusdesc LIKE ('%analysis%') THEN 7
+              ELSE 99 
+       END, 
+       Created
