2b0d60c6b121dd59f2511792b7dd190ec2320555
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-08-30 13:36:54 +0100

    Added re-openned issues controller

---
+++ GeminiBacklog/Controllers/ReopenedIssuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using GeminiBacklog.Controllers.DataAccess;
+
+namespace GeminiBacklog.Controllers
+{
+    public class ReopenedIssuesController : ApiController
+    {
+        static readonly string _sql = SqlQueries.GetSql("GeminiBacklog.Queries.ReopenedIssues.sql");
+
+        class ReopenedIssue
+        {
+            public int IssueId { get; set; }
+            public string Issue { get; set; }
+            public string Summary { get; set; }
+        }
+
+        [Route("issues/reopened/{dateFrom}")]
+        public dynamic Get(DateTime dateFrom)
+        {
+            var reopenedIssues = new DBWrapper().Query<ReopenedIssue>(_sql, new { dateFrom });
+
+            return
+                new
+                {
+                    count = reopenedIssues.Count(),
+                    issues = reopenedIssues
+                };
+        }
+    }
+}
---
+++ GeminiBacklog/Queries/ReopenedIssues.sql
@@ -0,0 +1,31 @@
+﻿SELECT i.issueid
+	,isnull(isl.statusdesc, REPLACE(history, 'Issue status changed to ', '')) AS [status]
+	,count(isl.statusid) AS StatusChangeCount
+INTO #IssueHistory
+FROM issuehistory ih
+JOIN issuestatuslut isl ON isl.statusdesc = REPLACE(history, 'Issue status changed to ', '')
+JOIN issues i ON i.issueid = ih.issueid
+WHERE i.projid = 14
+	AND isl.statusdesc = 'Closed'
+GROUP BY ih.issueid,ih.history,isl.statusdesc,i.issueid
+HAVING MAX(ih.created) >= @DateFrom
+
+SELECT DISTINCT d.issueid
+	,ISNULL(q5.StatusChangeCount, 0) AS 'Closed'
+INTO #StatusChangeCounts
+FROM #IssueHistory d
+	LEFT JOIN (SELECT issueid,StatusChangeCount FROM #IssueHistory WHERE [status] = 'Closed') AS q5 ON q5.issueid = d.issueid
+
+SELECT i.issueid
+    ,i.summary
+	,CASE
+		WHEN i.projid = 12 THEN 'T-' + CONVERT(NVARCHAR(10), i.issueid)
+		WHEN i.projid = 14 THEN 'CR-' + CONVERT(NVARCHAR(10), i.issueid)
+		ELSE 'PROB-' + CONVERT(NVARCHAR(10), i.issueid)
+	END AS Issue
+FROM #StatusChangeCounts c
+JOIN issues i ON i.issueid = c.issueid
+WHERE Closed > 1
+
+DROP TABLE #IssueHistory
+DROP TABLE #StatusChangeCounts
