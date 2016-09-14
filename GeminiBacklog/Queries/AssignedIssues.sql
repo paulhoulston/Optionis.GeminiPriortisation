d2c49693c6d9bd44a105bf635349c6b4c86ff24c
paul.houlston <paul.houlston@parasolgroup.co.uk>
2016-09-14 14:08:12 +0100

    Added 3rd line incident tickets and resolutiions issue to excluded issues in assigned to view

--- GeminiBacklog/Queries/AssignedIssues.sql
+++ GeminiBacklog/Queries/AssignedIssues.sql
@@ -19,5 +19,5 @@ JOIN dbo.issuetypelut itl on itl.typeid = i.isstype
 JOIN dbo.issueprioritylut p on p.priorityid = i.isspriority
 WHERE userid = @userId
  AND isl.statusdesc NOT IN ('Closed')
- AND i.issueid NOT IN (29015, 29017, 44227, 44238, 44239, 29459)
+ AND i.issueid NOT IN (29015, 29017, 44227, 44238, 44239, 29459, 31860)
 ORDER BY i.issueid
