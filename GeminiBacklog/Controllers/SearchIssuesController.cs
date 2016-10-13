ca3ba72a641f367e17af88fa34b7e97934e88d58
paul.houlston <paul.houlston@optionis.co.uk>
2016-10-13 11:05:13 +0100

    Added endpoint to search issues

---
+++ GeminiBacklog/Controllers/SearchIssuesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using GeminiBacklog.Controllers.DataAccess;
+using GeminiBacklog.Models;
+
+namespace GeminiBacklog.Controllers
+{
+    public class SearchIssuesController : ApiController
+    {
+        static readonly string _sql = SqlQueries.GetSql("GeminiBacklog.Queries.SearchIssues.sql");
+
+        public dynamic Get(string searchTerm)
+        {
+            return new { Issues = new DBWrapper().Query<IssueModel>(_sql, new { searchTerm }) };
+        }
+    }
+}
---
+++ GeminiBacklog/Queries/SearchIssues.sql
@@ -0,0 +1,33 @@
+﻿SELECT i.issueid AS IssueId
+	,i.projid AS Project
+	,i.summary AS Summary
+	,isl.statusdesc AS [Status]
+	,itl.typedesc AS [Type]
+	,i.duedate AS DueDate
+	,i.created AS Created
+	,p.seq AS [Priority]
+	,p.prioritydesc AS PriorityDesc
+	,CASE 
+		WHEN i.projid = 12
+			THEN 'Ticket'
+		WHEN i.projid = 14
+			THEN 'CR'
+		ELSE 'Problem'
+		END AS IssueType
+FROM dbo.issues i
+JOIN dbo.issuestatuslut isl ON isl.statusid = i.issstatus
+JOIN dbo.issuetypelut itl ON itl.typeid = i.isstype
+JOIN dbo.issueprioritylut p ON p.priorityid = i.isspriority
+WHERE i.issueid IN (
+		SELECT issueid
+		FROM dbo.issues
+		WHERE summary LIKE '%' + @searchTerm + '%'
+			OR longdesc LIKE '%' + @searchTerm + '%'
+		
+		UNION
+		
+		SELECT issueid
+		FROM dbo.issuecomments
+		WHERE comment LIKE '%' + @searchTerm + '%'
+		)
+
