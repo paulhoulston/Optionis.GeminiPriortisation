6a02dad69f8c388bfe1e8411ca6ea752c75d71cc
paul.houlston <paul.houlston@optionis.co.uk>
2016-11-01 11:02:26 +0000

    Updated model used by dev issues controller

---
+++ GeminiBacklog/Controllers/DevAssignedTasksController.cs
@@ -0,0 +1,14 @@
+﻿using GeminiBacklog.Models;
+using System.Web.Http;
+
+namespace GeminiBacklog.Controllers
+{
+    public class DevAssignedTasksController : ApiController
+    {
+        [Route("people/devissues")]
+        public dynamic Get()
+        {
+            return new { Issues = new BAUTaskModel[] { } };
+        }
+    }
+}
