240241cf85886fc2f06fa366dc05c2e156c705d7
paul.houlston <paul.houlston@optionis.co.uk>
2016-11-14 10:26:51 +0000

    Added dev user to dev assigned tasks

--- GeminiBacklog/Controllers/DevAssignedTasksController.cs
+++ GeminiBacklog/Controllers/DevAssignedTasksController.cs
@@ -9,11 +9,14 @@ namespace GeminiBacklog.Controllers
     {
         const string ROUTE = "devissues";
         static readonly GetIssues _issueGetter = new GetIssues("GeminiBacklog.Queries.DevAssigned.sql");
-        static readonly IEnumerable<int> _devUserIds = ConfigurationManager.AppSettings["GEMINI_DEV_USER_IDS"].Split(',').Select(id => int.Parse(id));
+        static readonly List<int> _devUserIds = ConfigurationManager.AppSettings["GEMINI_DEV_USER_IDS"].Split(',').Select(id => int.Parse(id)).ToList();
 
         [Route(ROUTE)]
         public dynamic Get(string filter = "")
         {
+            // Add the dev user to the IDs list
+            _devUserIds.Add(39);
+            
             return _issueGetter.Get(ROUTE, new { userIds = _devUserIds, filter });
         }
     }
